68a7284665be91e099a45ba7cd484c3c8b3f08c5
Abubakarashrafi <abubakar16350@gmail.com>
2024-07-24 03:10:51 +0500

    added player names

--- match.js
+++ match.js
@@ -1,9 +1,31 @@
-let team1 = "Pakistan";
-let team2 = "India";
+let team1 = "pakistan";
+let team2 = "india";
 let overs = 2;
 let secondInning;
 let score = 0;
 let wkts = 0;
+const players = {
+    pakistan:{
+        batsmen:[
+            "Mohammed Rizwan","Babar Azam","Fakhar Zaman","Shadab Khan","Azam Khan", "Iftikhar Ahmed"
+        ],
+        bowler : [
+            "Haris Rauf","Naseem shah","Mohammed Amir","Iftikhar Ahmed","Shadab Khan"
+        ]
+
+    },
+    india:{
+        batsmen: [
+            "Virat Kholi", "Rohit Sharma", "Shikhar dhawan", "Shubman gill", "Hardik pandya", "Ravindra jadeja"
+        ],
+        bowler: [
+            "Mohammed Shami", "Jasprit bhumra", "Kuldeep yadav", "Shardul thakur", "Navdeep saini"
+        ]
+    }
+    
+}
+
+
 
 
 function sleepFor(duration) {
@@ -15,29 +37,39 @@ function sleepFor(duration) {
 
 
 
+
 const match = (team1, team2, overs)=> {
     sleepFor(1000);
     console.log(`The match is between ${team1} and ${team2}`);
     let battingTeam;
+    let bowlingTeam;
     if(toss("heads",team1,team2)){
         battingTeam = team1;
+        bowlingTeam = team2;
         console.log(`${team1} won the toss and choose to bat first`);
+
     }
     else{
         battingTeam = team2;
+        bowlingTeam = team1;
         console.log(`${team2} won the toss and choose to bat first`);
     } 
     sleepFor(2000)
     console.log(`${overs} Overs match started, ${battingTeam} is to bat`);
-    innings(battingTeam);
+    innings(battingTeam,bowlingTeam);
     secondInning = true;
 
-    if(battingTeam === team1)
+    if(battingTeam === team1){
         battingTeam = team2 
-    else 
-        battingTeam = team1
+        bowlingTeam = team1;
+    }
 
-    innings(battingTeam);
+    else {
+        bowlingTeam = team2;
+        battingTeam = team1
+    }
+
+    innings(battingTeam,bowlingTeam);
 }
 
 const toss = (choice,team1,team2)=> {
@@ -55,12 +87,20 @@ const toss = (choice,team1,team2)=> {
 let target;
 const ScoreCalc = (team,overs)=> {
     let totalballs = 6;
+    
+
     while (totalballs--) {
          let number = Math.floor(Math.random() * 7);
           console.log(number);
         if(number==0){
             console.log("its out!");
-            wkts++;
+            sleepFor(1000)
+            console.log(`${players[team].batsmen[wkts]} got out`);
+            sleepFor(2000);
+            wkts++
+           
+            let player = players[team].batsmen[wkts]
+           console.log(`${player} to bat`);
             sleepFor(2000);
         }else if(number == 4 || number == 6){
             console.log(`its ${number}`);
@@ -82,7 +122,7 @@ const ScoreCalc = (team,overs)=> {
 
 }
 
-const innings = (team)=> {
+const innings = (team,bowl)=> {
         let i=1;
         totalScore = score
         score = 0;
@@ -90,7 +130,7 @@ const innings = (team)=> {
          totalOvers = overs ;
         if(secondInning){
             console.log(
-                `To Win ${team} need ${totalScore - score+1} From ${
+                `To Win ${team} need ${totalScore - score+1} runs From ${
                     overs 
                 } Overs`);
                 sleepFor(2000);
@@ -98,9 +138,15 @@ const innings = (team)=> {
             }
         while(totalOvers--){
         sleepFor(2000)
-       
+
                 console.log(`Over # ${i++} started `);
                 sleepFor(1000);
+            let player = players[team].batsmen[wkts]
+            let bowlers = players[bowl].bowler[i]
+            
+               console.log(`${player} to bat`);
+               sleepFor(2000)
+               console.log(`${bowlers} to bowl`);
                 ScoreCalc(team,totalOvers);
                 sleepFor(1000);
                 console.log(`Team ${team} Score: ${score}/${wkts}`);
@@ -110,18 +156,20 @@ const innings = (team)=> {
         
    const winningCondition = (team, totalScore, score, overs) => {
   sleepFor(1000);
-    console.log(overs + "overs:");
-  if (score < totalScore && overs ==0) {
-        console.log(`${team} lost by ${totalScore - score} runs`);
-        return true;
-  } else if (score > totalScore) {
+  if(secondInning){
+
+      if (score < totalScore && overs ==0) {
+          console.log(`${team} lost by ${totalScore - score} runs`);
+          return true;
+        } else if (score > totalScore) {
     console.log(`${team} won by ${10 - wkts} wkts`);
     return true;
+}
 }
 };
 
 
 
 
-match(team1,team2,overs);
+ match(team1,team2,overs);
 
